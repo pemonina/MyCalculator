6e836fe8cf4be44eb0d5198c66a50157cdb7e5fc
Philips Emonina <emoninaphilips@gmail.com>
2013-04-11 13:02:08 +0100

    Initial commit

---
+++ MyCalculator/src/ie/gcd/maths/Calculator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ie.gcd.maths;
+
+/**
+ * @author olayinka adegun
+ *
+ */
+public class Calculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
