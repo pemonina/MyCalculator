24c0cbb9e84403a3e17ffce7617e4f03a5f32fe4
Philips Emonina <emoninaphilips@gmail.com>
2013-04-11 13:20:54 +0100

    Branch setup

---
+++ MyCalculator/src/Test/ScientificCalculatorTest.java
@@ -0,0 +1,14 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScientificCalculatorTest {
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ MyCalculator/src/ie/gcd/maths/ScientificCalculator.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package ie.gcd.maths;
+
+/**
+ * @author olayinka adegun
+ *
+ */
+public class ScientificCalculator {
+
+public int add(int x, int y){
+return x-y;
+}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
